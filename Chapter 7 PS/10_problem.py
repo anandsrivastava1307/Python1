c29fcf8e5366af9ff8dad4fa37358aca03e382a0
Anand Srivastava <anandsri13796@gmail.com>
2026-05-30 02:15:39 +0530

    my code

--- Chapter 7 PS/10_problem.py
+++ Chapter 7 PS/10_problem.py
@@ -1,6 +1,8 @@
 #Questioon 10 : Write a program to print multiplication table if n using for loops in reversed order.
-
-n = int(input("Enter your number : "))
-
-for i in range(1,11):
+# Method 1 — Pehla wala (thoda tricky)
+for i in range(1, 11):
     print(f"{n} X {11 - i} = {n*(11-i)}")
+
+# Method 2 — Ye wala (clean & simple ✅)
+for i in range(10, 0, -1):
+    print(f"{n} X {i} = {n*i}")
